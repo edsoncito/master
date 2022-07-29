f23839cd9520535789e09710cf91b0ad8e6841d5
edsoncito <edson.nur@gmail.com>
2022-07-28 22:25:11 -0400

    dd

--- ProyectoFinal-CheckIn/Domain/src/main/java/Repositories/IcheckInRepository.java
+++ ProyectoFinal-CheckIn/Domain/src/main/java/Repositories/IcheckInRepository.java
@@ -1,19 +1,16 @@
 package Repositories;
 
+import Modal.CheckIn;
+import core.IRepository;
 import java.util.List;
 import java.util.UUID;
 
-import Modal.CheckIn;
-import core.IRepository;
-
 public interface IcheckInRepository extends IRepository<CheckIn, UUID> {
+	// public void UpdateAsync(CheckIn obj);
 
-    //public void UpdateAsync(CheckIn obj);
+	public List<CheckIn> GetAll();
 
-    public List<CheckIn> GetAll();
+	public CheckIn Delete(CheckIn checkIn);
 
-    public CheckIn Delete(CheckIn checkIn);
-
-    public CheckIn Update(CheckIn checkIn);
-
+	public CheckIn Update(CheckIn checkIn);
 }
