5530c626a6a7f42134a9dea91f2aa222f288b7c3
edsoncito <edson.nur@gmail.com>
2022-07-28 22:04:53 -0400

    asdd

--- ProyectoFinal-CheckIn/Aplication/src/main/java/UseCases/Command/CheckIn/Create/CrearCheckInHandler.java
+++ ProyectoFinal-CheckIn/Aplication/src/main/java/UseCases/Command/CheckIn/Create/CrearCheckInHandler.java
@@ -4,36 +4,49 @@ import Modal.CheckIn;
 import Repositories.IUnitOfWork;
 import Repositories.IcheckInRepository;
 import Services.CheckInServices;
-import UseCases.Command.CheckIn.Create.CrearCheckInCommand;
 import factories.ICheckInFactory;
 import fourteam.http.Exception.HttpException;
 import fourteam.mediator.RequestHandler;
 
-public class CrearCheckInHandler implements RequestHandler<CrearCheckInCommand, CheckIn> {
+public class CrearCheckInHandler
+	implements RequestHandler<CrearCheckInCommand, CheckIn> {
 
-    private IcheckInRepository CheckInRepository;
-    private ICheckInFactory CheckInFactory;
-    private CheckInServices inService;
-    private IUnitOfWork _unitOfWork;
+	private IcheckInRepository CheckInRepository;
+	private ICheckInFactory CheckInFactory;
+	private CheckInServices inService;
+	private IUnitOfWork _unitOfWork;
 
-    public CrearCheckInHandler(IcheckInRepository checkInRepository, ICheckInFactory checkInFactory, IUnitOfWork unitOfWork,CheckInServices checkInServices  ) {
-        CheckInRepository = checkInRepository;
-        CheckInFactory = checkInFactory;
-        _unitOfWork = unitOfWork;
-        inService = checkInServices;
-    }
+	public CrearCheckInHandler(
+		IcheckInRepository checkInRepository,
+		ICheckInFactory checkInFactory,
+		IUnitOfWork unitOfWork,
+		CheckInServices checkInServices
+	) {
+		CheckInRepository = checkInRepository;
+		CheckInFactory = checkInFactory;
+		_unitOfWork = unitOfWork;
+		inService = checkInServices;
+	}
 
-    @Override
-    public CheckIn handle(CrearCheckInCommand request) throws HttpException {
-
-        String nroCheckIn = inService.GenerarNroPedidoAsync();
-        CheckIn objCheckIn = CheckInFactory.Create(nroCheckIn, request.checkInDto.getEstadoPaciente(), request.checkInDto.getDescripcion() ,request.checkInDto.getAsiento());
-        for (var item: request.Equipaje) {
-            objCheckIn.AgregarItem(item.getPesoEquipaje(), item.getNumeroEtiqueta(), item.getDescripcion());
-        }
-        objCheckIn.checkInCompletado();
-        CheckInRepository.Create(objCheckIn);
-        _unitOfWork.commit();
-        return objCheckIn;
-    }
+	@Override
+	public CheckIn handle(CrearCheckInCommand request) throws HttpException {
+		String nroCheckIn = inService.GenerarNroPedidoAsync();
+		CheckIn objCheckIn = CheckInFactory.Create(
+			nroCheckIn,
+			request.checkInDto.getEstadoPaciente(),
+			request.checkInDto.getDescripcion(),
+			request.checkInDto.getAsiento()
+		);
+		for (var item : request.Equipaje) {
+			objCheckIn.AgregarItem(
+				item.getPesoEquipaje(),
+				item.getNumeroEtiqueta(),
+				item.getDescripcion()
+			);
+		}
+		objCheckIn.checkInCompletado();
+		CheckInRepository.Create(objCheckIn);
+		_unitOfWork.commit();
+		return objCheckIn;
+	}
 }
