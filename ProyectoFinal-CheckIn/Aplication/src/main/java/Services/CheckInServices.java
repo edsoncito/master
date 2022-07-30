671eaba8b28c86a074f35dc5a4713788a8a7fed2
edsoncito <edson.nur@gmail.com>
2022-07-30 10:51:55 -0400

    vvv

--- ProyectoFinal-CheckIn/Aplication/src/main/java/Services/CheckInServices.java
+++ ProyectoFinal-CheckIn/Aplication/src/main/java/Services/CheckInServices.java
@@ -4,20 +4,20 @@ import java.util.Random;
 
 public class CheckInServices implements IcheckInService {
 
-	@Override
-	public String GenerarNroPedidoAsync() {
-		String val = "";
-		Random random = new Random();
-		for (int i = 0; i < 10; i++) {
-			String charOrNum = random.nextInt(2) % 2 == 0 ? "Char" : "num";
-			if ("char".equalsIgnoreCase(charOrNum)) {
-				int choice = random.nextInt(2) % 2 == 0 ? 65 : 97;
-				val += (char) (choice + random.nextInt(26));
-			}
-			if ("num".equalsIgnoreCase(charOrNum)) {
-				val += String.valueOf(random.nextInt(10));
-			}
-		}
-		return val;
-	}
+  @Override
+  public String GenerarNroPedidoAsync() {
+    String val = "";
+    Random random = new Random();
+    for (int i = 0; i < 10; i++) {
+      String charOrNum = random.nextInt(2) % 2 == 0 ? "Char" : "num";
+      if ("char".equalsIgnoreCase(charOrNum)) {
+        int choice = random.nextInt(2) % 2 == 0 ? 65 : 97;
+        val += (char) (choice + random.nextInt(26));
+      }
+      if ("num".equalsIgnoreCase(charOrNum)) {
+        val += String.valueOf(random.nextInt(10));
+      }
+    }
+    return val;
+  }
 }
