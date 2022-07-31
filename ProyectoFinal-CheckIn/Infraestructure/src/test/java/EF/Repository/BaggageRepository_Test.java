616b2ef731703b867a469d2078e0ad6d2ce60cba
edsoncito <edson.nur@gmail.com>
2022-07-30 23:37:17 -0400

    new

--- ProyectoFinal-CheckIn/Infraestructure/src/test/java/EF/Repository/BaggageRepository_Test.java
+++ ProyectoFinal-CheckIn/Infraestructure/src/test/java/EF/Repository/BaggageRepository_Test.java
@@ -1,77 +1,73 @@
 package EF.Repository;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import EF.Contexts.IWriteDbContext;
 import EF.Contexts.MongoDb.WriteDbContext;
 import Modal.Baggage;
 import fourteam.db.DbSet;
 import fourteam.http.Exception.HttpException;
+import java.util.UUID;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import static org.mockito.ArgumentMatchers.any;
-import java.util.UUID;
-
 
 public class BaggageRepository_Test {
 
-    @Before
-    public void setup() {
-    }
+  @Before
+  public void setup() {}
 
-    IWriteDbContext bdTest = Mockito.mock(IWriteDbContext.class);
-    DbSet<Baggage> _baggage = Mockito.mock(DbSet.class);
+  IWriteDbContext bdTest = Mockito.mock(IWriteDbContext.class);
+  DbSet<Baggage> _baggage = Mockito.mock(DbSet.class);
 
-    @Before
-    public void setUp() {
-        bdTest.baggage = _baggage;
-    }
+  @Before
+  public void setUp() {
+    bdTest.baggage = _baggage;
+  }
 
-    @Test
-    public void HandleBaggageRegistrado_Ok() throws HttpException {
+  @Test
+  public void HandleBaggageRegistrado_Ok() throws HttpException {
+    Double PesoEquipaje = 23.3;
+    String NumeroEtiqueta = "sdfd33f3";
+    String Descripcion = "sdsfsfse";
+    IWriteDbContext bdTest = new WriteDbContext();
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    Baggage objBaggage = new Baggage(PesoEquipaje, NumeroEtiqueta, Descripcion);
+  }
 
-        Double PesoEquipaje = 23.3;
-        String NumeroEtiqueta = "sdfd33f3";
-        String Descripcion = "sdsfsfse";
+  @Test
+  public void GetAll_Test() {
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    baggageRepository.GetAll();
+    Assert.assertNotNull(baggageRepository);
+  }
 
-        IWriteDbContext bdTest = new WriteDbContext();
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        Baggage objBaggage = new Baggage(PesoEquipaje, NumeroEtiqueta, Descripcion);
-    }
+  @Test
+  public void Create_Test() {
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    baggageRepository.Create(any(Baggage.class));
+    Assert.assertNotNull(baggageRepository);
+  }
 
-    @Test
-    public void GetAll_Test() {
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        baggageRepository.GetAll();
-        Assert.assertNotNull(baggageRepository);
-    }
+  @Test
+  public void Update_Test() {
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    baggageRepository.Update(new Baggage());
+    Assert.assertNotNull(baggageRepository);
+  }
 
-    @Test
-    public void Create_Test(){
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        baggageRepository.Create(any(Baggage.class));
-        Assert.assertNotNull(baggageRepository);
-    }
+  @Test
+  public void Delete_Test() {
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    baggageRepository.Delete(new Baggage());
+    Assert.assertNotNull(baggageRepository);
+  }
 
-    @Test
-    public void Update_Test(){
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        baggageRepository.Update(new Baggage());
-        Assert.assertNotNull(baggageRepository);
-    }
-
-    @Test
-    public void Delete_Test(){
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        baggageRepository.Delete(new Baggage());
-        Assert.assertNotNull(baggageRepository);
-    }
-
-    @Test
-    public void FindByKey_Test(){
-        BaggageRepository baggageRepository = new BaggageRepository(bdTest);
-        baggageRepository.FindByKey(UUID.randomUUID());
-        Assert.assertNotNull(baggageRepository);
-    }
-
+  @Test
+  public void FindByKey_Test() {
+    BaggageRepository baggageRepository = new BaggageRepository(bdTest);
+    baggageRepository.FindByKey(UUID.randomUUID());
+    Assert.assertNotNull(baggageRepository);
+  }
 }
--- ProyectoFinal-CheckIn/SharedKernel/src/test/java/core/Entity_Test.java
+++ ProyectoFinal-CheckIn/SharedKernel/src/test/java/core/Entity_Test.java
@@ -9,29 +9,28 @@ import org.mockito.Mockito;
 
 public class Entity_Test {
 
-	@Test
-	public void constructor_accept() {
-		Entity entity = new Entity() {};
-		entity.setKey(UUID.randomUUID());
-		entity.getKey();
-		entity.addDomainEvent(new DomainEvent());
-		entity.getDomainEvents();
-		entity.clearDomainEvent();
-		BussinessRule rule = Mockito.mock(BussinessRule.class);
+  @Test
+  public void constructor_accept() {
+    Entity entity = new Entity() {};
+    entity.setKey(UUID.randomUUID());
+    entity.getKey();
+    entity.addDomainEvent(new DomainEvent());
+    entity.getDomainEvents();
+    entity.clearDomainEvent();
+    BussinessRule rule = Mockito.mock(BussinessRule.class);
 
-		try {
-			when(rule.IsValid()).thenReturn(false);
-			entity.CheckRule(rule);
-		} catch (BussinessRuleValidateExeption e) {}
-		try {
-			entity.CheckRule(null);
-		} catch (Exception e) {}
+    try {
+      when(rule.IsValid()).thenReturn(false);
+      entity.CheckRule(rule);
+    } catch (BussinessRuleValidateExeption e) {}
+    try {
+      entity.CheckRule(null);
+    } catch (Exception e) {}
 
-		try {
-			when(rule.IsValid()).thenReturn(false);
-			entity.CheckRule(rule);
-		} catch (BussinessRuleValidateExeption e) {}
-
-		Assert.assertNotNull(entity);
-	}
+    try {
+      when(rule.IsValid()).thenReturn(false);
+      entity.CheckRule(rule);
+    } catch (BussinessRuleValidateExeption e) {}
+    Assert.assertNotNull(entity);
+  }
 }
